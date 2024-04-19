dc7869df75f4af2137be97250f5f1adfc2fb3701
izzy-2023 <izzyzinxhirija@gmail.com>
2024-04-19 17:27:18 -0400

    can make GraphQL API calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+
+const GET_ARTICLES = gql`
+  {
+    articles {
+      id
+      title
+      body
+      publishedDate
+    }
+  }
+`;
+
+const CREATE_ARTICLE = gql`
+  mutation CreateArticle($title: String!, $body: String!) {
+    createArticle(title: $title, body: $body) {
+      article {
+        id
+        title
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_ARTICLES);
+  const [createArticle] = useMutation(CREATE_ARTICLE);
+
+  const handleCreateArticle = () => {
+    const title = prompt("Enter article title:");
+    const body = prompt("Enter article content:");
+    if (title && body) {
+      createArticle({ variables: { title, body } });
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Articles</h1>
+      {data.articles.map(({ id, title, body }) => (
+        <div key={id}>
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+      ))}
+      <button onClick={handleCreateArticle}>Add Article</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/',
+  cache: new InMemoryCache()
+});
+
+const container = document.getElementById('root'); // Get the root container
+const root = createRoot(container); // Create a root
+
+// Use the root to render your component tree
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// For measuring performance
 reportWebVitals();
