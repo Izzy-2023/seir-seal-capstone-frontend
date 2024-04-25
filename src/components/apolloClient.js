da02c8425fad20685e38f29bb5926b1246f8ebed
izzy-2023 <izzyzinxhirija@gmail.com>
2024-04-24 22:15:21 -0400

    updated uri path for the apolloClient file

--- src/components/apolloClient.js
+++ src/components/apolloClient.js
@@ -3,6 +3,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql/',
+  uri: 'https://cms-backend-toox.onrender.com/graphql/',
   cache: new InMemoryCache()
 });
