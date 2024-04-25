ebaefb0b6198147bb16c861bd3fb9be3a985645c
izzy-2023 <izzyzinxhirija@gmail.com>
2024-04-24 23:31:04 -0400

    added hamburger menu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,33 @@
 // src/components/Dashboard.js
 
-import React from 'react';
-import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
 import InboxIcon from '@mui/icons-material/Inbox';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 
 const Dashboard = ({ children }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div style={{ display: 'flex' }}>
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleMenuToggle}
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" noWrap>
             CMS Dashboard
           </Typography>
@@ -19,17 +35,19 @@ const Dashboard = ({ children }) => {
       </AppBar>
       <Drawer variant="permanent" sx={{ width: drawerWidth, [`& .MuiDrawer-paper`]: { width: drawerWidth } }}>
         <Toolbar />
-        <List>
-          <ListItem button component={Link} to="/">
-            <ListItemIcon><InboxIcon /></ListItemIcon>
-            <ListItemText primary="Articles" />
-          </ListItem>
-          <ListItem button component={Link} to="/add-post">
-            <ListItemIcon><InboxIcon /></ListItemIcon>
-            <ListItemText primary="Add Post" />
-          </ListItem>
-          {/* ... other navigation items */}
-        </List>
+        {menuOpen && (
+          <List>
+            <ListItem button component={Link} to="/">
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary="Articles" />
+            </ListItem>
+            <ListItem button component={Link} to="/add-post">
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary="Add Post" />
+            </ListItem>
+            {/* ... other navigation items */}
+          </List>
+        )}
       </Drawer>
       <main style={{ flexGrow: 1, p: 3, width: `calc(100% - ${drawerWidth}px)` }}>
         <Toolbar />
@@ -40,3 +58,4 @@ const Dashboard = ({ children }) => {
 };
 
 export default Dashboard;
+
